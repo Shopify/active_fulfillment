d1b8b5750b2ea8bc13eb33994bd8acfb65b98f5f
csaunders <chris@christophersaunders.ca>
2012-03-30 10:46:56 -0400

    Did a bit of refactoring and started working on fulfillment creation.

--- lib/active_fulfillment/fulfillment/services/amazon_mws.rb
+++ lib/active_fulfillment/fulfillment/services/amazon_mws.rb
@@ -11,7 +11,7 @@ module ActiveMerchant
 
       SIGNATURE_VERSION = 2
       SIGNATURE_METHOD  = "SHA256"
-      VERSION = "2009-01-01"
+      VERSION = "2011-01-01"
 
       ENDPOINTS = {
         :ca => 'mws.amazonservices.ca',
@@ -65,18 +65,47 @@ module ActiveMerchant
         ENDPOINTS[@options[:endpoint] || :us]
       end
 
-      def sign(http_verb, request_uri, options)
-        opts = prepare_for_signing(options)
+      def fulfill(order_id, shipping_address, line_items, options = {})
+        requires!(options, :order_date, :comment, :shipping_method)
+        commit :post, :outbound, :create, build_fulfillment_request(order_id, shipping_address, line_items, options)
+      end
+
+      def commit(verb, service, op, params)
+        uri = URI.parse("#{endpoint}/#{OPERATIONS[service][op]}/#{VERSION}")        
+        signature = sign(http_verb, uri, params)
+        query = build_query(params) + "&Signature=#{signature}"
+        headers = construct_headers(query)
+        response = ssl_post(uri.to_s, query, headers)
+      end
+
+      def sign(http_verb, uri, options)
+        opts = build_basic_api_query(options)
         string_to_sign = "#{http_verb.to_s.upcase}\n"
-        string_to_sign += "#{endpoint}\n"
-        string_to_sign += "#{request_uri}\n"
-        string_to_sign += opts.sort.map{ |key,value| [CGI.escape(key.to_s), CGI.escape(value.to_s)].join('=') }.join('&')
+        string_to_sign += "#{uri.host}\n"
+        string_to_sign += uri.path.length <= 0 ? "/\n" : "#{uri.path}\n"
+        string_to_sign += build_query(options)
         
-        #remove trailing newline created by encode64
+        # remove trailing newline created by encode64
         Base64.encode64(OpenSSL::HMAC.digest(SIGNATURE_METHOD, @options[:password], string_to_sign)).chomp
       end
 
-      def prepare_for_signing(options)
+      def md5_content(content)
+        Base64.encode64(OpenSSL::Digest::Digest.new('md5', content).digest).chomp
+      end
+
+      def build_query(query_params)
+        query_params.sort.map{ |key, value| [CGI.escape(key.to_s), CGI.escape(value.to_s)].join('=') }.join('&')
+      end
+
+      def build_headers(querystr)
+        {
+          'User-Agent' => APPLICATION_IDENTIFIER,
+          'Content-MD5' => md5_content(querystr),
+          'Content-Type' => 'application/x-www-form-urlencoded'
+        }
+      end
+
+      def build_basic_api_query(options)
         opts = options.dup
         opts["AWSAccessKeyId"] = @options[:login] unless opts["AWSAccessKey"]
         opts["Timestamp"] = Time.now.utc.iso8601 unless opts["Timestamp"]
@@ -84,6 +113,20 @@ module ActiveMerchant
         opts["SignatureMethod"] = "Hmac#{SIGNATURE_METHOD}" unless opts["SignatureMethod"]
         opts["SignatureVersion"] = SIGNATURE_VERSION unless opts["SignatureVersion"]
         opts
+      end
+
+      private
+      def build_fulfillment_request(order_id, shipping_address, line_items, options)
+        params = {
+          :Action => OPERATIONS[:outbound][:create],
+          :SellerFulfillmentOrderId => order_id.to_s,
+          :DisplayableOrderId => order_id.to_s
+        }
+        request = build_basic_api_query(params.merge(options))
+        request.concat build_address(shipping_address)
+        request.concat build_items(line_items)
+
+        request
       end
     end
   end
