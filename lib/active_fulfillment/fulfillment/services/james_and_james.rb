ed57927d2f71dc5c32b3bd56f062088e7abe1055
Dan Wilson <dan@sugru.com>
2014-06-24 12:21:31 +0100

    Line item formatting

--- lib/active_fulfillment/fulfillment/services/james_and_james.rb
+++ lib/active_fulfillment/fulfillment/services/james_and_james.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'cgi'
 
 module ActiveMerchant
   module Fulfillment
@@ -20,18 +21,50 @@ module ActiveMerchant
       private
 
       def build_fulfillment_request(order_id, shipping_address, line_items, options)
-        # build hash of data
-        {}
+        {
+          client_ref: order_id,
+          ShippingContact: format_address(shipping_address),
+          BillingContact: format_address(shipping_address),
+          items: format_line_items(line_items)
+        }
       end
 
-      def commit(action, request)
-        data = ssl_post(SERVICE_URLS[action], request)
+      def commit(action, data)
+        request = {api_key: @options[:key], test: true, order: data}
+        data = ssl_post(SERVICE_URLS[action], JSON.generate(request))
         response = parse_response(action, data)
         Response.new(response["success"], "message", response)
       end
 
       def parse_response(action, json)
         JSON.parse(json)
+      end
+
+      def format_address(address)
+        data = {
+          name: address[:name],
+          address: address[:address1],
+          city: address[:city],
+          country: address[:country],
+          postcode: address[:zip]
+        }
+        data[:company] = address[:company] unless address[:company].blank?
+        data[:email] = address[:email] unless address[:email].blank?
+        data[:address_contd] = address[:address2] unless address[:address2].blank?
+        data[:country] = address[:state] unless address[:state].blank?
+        return data
+      end
+
+      def format_line_items(items)
+        data = []
+        items.each do |item|
+          data << {
+            client_ref: item[:sku],
+            quantity: item[:quantity],
+            price: item[:price]
+          }
+        end
+        return data
       end
 
     end
