28b5bc966d1ef45d7063d0cd495d359015d8bbe7
Willem van Bergen <willem@vanbergen.org>
2015-01-08 11:24:41 -0500

    Fix Minitest for activesupport 4.0

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,12 @@ require 'timecop'
 require 'logger'
 ActiveFulfillment::Service.logger = Logger.new(nil)
 
+# This makes sure that Minitest::Test exists when an older version of Minitest
+# (i.e. 4.x) is required by ActiveSupport.
+unless defined?(Minitest::Test)
+  Minitest::Test = MiniTest::Unit::TestCase
+end
+
 module ActiveFulfillment::Test
   module Fixtures
     LOCAL_CREDENTIALS = ENV['HOME'] + '/.active_fulfillment/fixtures.yml' unless defined?(LOCAL_CREDENTIALS)
