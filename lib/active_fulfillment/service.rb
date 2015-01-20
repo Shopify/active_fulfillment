05f70085b67996d3795767fcf1b3c312e110c876
Kevin Hughes <kevin.hughes@shopify.com>
2015-01-20 18:24:32 +0000

    update yardoc input params

--- lib/active_fulfillment/service.rb
+++ lib/active_fulfillment/service.rb
@@ -29,14 +29,31 @@ module ActiveFulfillment
     #
     # @example
     #
-    #     carrier = ActiveFulfillment::Carrier.new(...)
-    #     response = carrier.fulfill(order.id, order.shipping_address, order.line_items)
+    #     service = ActiveFulfillment::Service.new(...)
+    #     response = service.fulfill(order.id, order.shipping_address, order.line_items)
     #     assert response.success?
     #
     #
-    # @param order_id [String] Unique identifier for the order
-    # @param shipping_address [?] The address to ship the order to
-    # @param line_items [?] The line items to include in the order.
+    # @param [String] order_id Unique identifier for the order
+    #
+    # @param [Hash] shipping_address The address to ship the order to
+    # @option shipping_address [String] :name
+    # @option shipping_address [String] :company
+    # @option shipping_address [String] :address1
+    # @option shipping_address [String] :address2
+    # @option shipping_address [String] :phone
+    # @option shipping_address [String] :city
+    # @option shipping_address [String] :state
+    # @option shipping_address [String] :country
+    # @option shipping_address [String] :zip
+    #
+    # @param [Array<Hash>] line_items The line items to include in the order.
+    # @option line_items [String] :sku
+    # @option line_items [String] :quantity
+    # @option line_items [String] :description
+    # @option line_items [String] :value
+    # @option line_items [String] :currency_code
+    #
     # @param options [Hash] Service-specific options.
     #
     # @return [ActiveFulfillment::FulfillResponse]
@@ -48,9 +65,10 @@ module ActiveFulfillment
     # Retrieve stock levels for a single SKU that the fulfillment service stocks.
     # The stock levels will be returned as a Hash-like object, indexed by SKU.
     #
-    # @param sku [Array<String>] The SKUs to retreive. All the provided skus
-    #   must be set as a key in the response object.
-    # @param options [Hash] Service-specific options.
+    # @param [Array<String>] sku The SKUs to retreive. All the provided skus
+    # must be set as a key in the response object.
+    # @param [Hash] options Service-specific options.
+    #
     # @return [ActiveFulfillment::StockLevelResponse]
     # @note This should be implemented by concrete subclasses.
     def fetch_stock_levels(skus, options = {})
@@ -62,11 +80,12 @@ module ActiveFulfillment
     #
     # @example
     #
-    #     carrier = ActiveFulfillment::Carrier.new(...)
-    #     stock_levels = carrier.fetch_all_stock_levels
+    #     service = ActiveFulfillment::Service.new(...)
+    #     stock_levels = service.fetch_all_stock_levels
     #     stock_levels.each { |sku, stock| ... }
     #
-    # @param options [Hash] Service-specific options.
+    # @param [Hash] options Service-specific options.
+    #
     # @return [ActiveFulfillment::StockLevelResponse]
     # @note This should be implemented by concrete subclasses.
     def fetch_all_stock_levels(options = {})
@@ -77,14 +96,15 @@ module ActiveFulfillment
     #
     # @example
     #
-    #     carrier = ActiveFulfillment::Carrier.new(...)
-    #     tracking_data = carrier.fetch_tracking_data([order.id])
+    #     service = ActiveFulfillment::Service.new(...)
+    #     tracking_data = service.fetch_tracking_data([order.id])
     #     tracking_data['order_id'].tracking_code
     #     tracking_data['order_id'].tracking_url
     #     tracking_data['order_id'].company
     #
-    # @param options [Array<String>] A list of order IDs for which to retrieve tracking options.
-    # @param options [Hash] Service-specific options.
+    # @param [Array<String>] order_ids A list of order IDs for which to retrieve tracking options.
+    # @param [Hash] options Service-specific options.
+    #
     # @return [ActiveFulfillment::TrackingDataResponse]
     # @note This should be implemented by concrete subclasses.
     def fetch_tracking_data(order_ids, options = {})
